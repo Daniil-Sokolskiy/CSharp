7664e4fd412a8470f2c48225594eda982b883d9e
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2022-11-18 23:53:53 +0400

    change_1

--- LECTION1/Delenie/Program.cs
+++ LECTION1/Delenie/Program.cs
@@ -1,3 +1,5 @@
-﻿double number1 = 10;
-double number2 = 4;
+﻿double number1 = new Random().Next(-100,101);
+Console.WriteLine(number1);
+double number2 = new Random().Next(-100,101);
+Console.WriteLine(number2);
 Console.WriteLine(number1/number2);
