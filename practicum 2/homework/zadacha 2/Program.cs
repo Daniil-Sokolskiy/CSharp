05f27644b373c162eed9ebcb3e9ccf01881679b0
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2022-11-20 01:57:18 +0400

    dz 2

---
+++ practicum 2/homework/zadacha 2/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+
+int num = new Random().Next(0,10000);
+Console.WriteLine(num);
+if(num>99)
+{
+string chislo = num.ToString();
+Console.WriteLine(chislo[2]);
+}
+else Console.WriteLine("Третьей цифры нет");
