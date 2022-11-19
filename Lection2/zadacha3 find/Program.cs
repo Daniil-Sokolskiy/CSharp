fca7f7763c41df6f2e537f6a2db7aa1037098908
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2022-11-19 22:20:14 +0400

    zadacha 3

---
+++ Lection2/zadacha3 find/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 23, 43, 67, 75, 34, 85, 66, 1, 5, 85, 92 };
+int n = array.Length;
+int find = 85;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
