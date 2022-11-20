f19d7c25c5ae8a92a32decf5fb7c28a4a4dd1b61
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2022-11-20 19:11:34 +0400

    dz 3

---
+++ Practicum 3/Homework/zadacha 2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 100);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+}
+
+int[] a = new int[3];
+FillArray(a);
+Console.Write($"Координаты точки а (");
+PrintArray(a);
+Console.WriteLine(")");
+
+int[] b = new int[3];
+FillArray(b);
+Console.Write($"Координаты точки b (");
+PrintArray(b);
+Console.WriteLine(")");
+
+double x = Math.Pow(a[0] - b[0], 2) + Math.Pow(a[1] - b[1], 2) + Math.Pow(a[2] - b[2], 2);
+
+double r = Math.Sqrt(x);
+Console.WriteLine(r);
---
+++ Practicum 3/Homework/zadacha 3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write($"Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Таблица кубов чисел до числа N");
+for (int i = 0; i<=N; i++)
+{
+double x = Math.Pow(i, 3);
+Console.WriteLine($"| {x} |");
+Console.WriteLine("------");
+}
