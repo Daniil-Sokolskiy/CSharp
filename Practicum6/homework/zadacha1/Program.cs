bbed60283480331d15d17b0eddd7a2aaa067692f
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2022-12-04 02:36:08 +0400

    Practicum6

--- Practicum6/homework/zadacha1/Program.cs
+++ Practicum6/homework/zadacha1/Program.cs
@@ -2,3 +2,16 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+Console.Write("Введите элементы (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
+//int count = 0;
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Кол-во элементов > 0: {count}");
