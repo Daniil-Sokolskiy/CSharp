5ff1812dd13cb1f9bae9e94a2f9d4bdcb515d6b2
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2022-12-11 01:33:53 +0400

    homework 8 undone

---
+++ practicum8/homework/zadacha1/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 21);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Sort(int[,] matrixxx)
+{
+    for (int i = 0; i < matrixxx.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrixxx.GetLength(1); j++)
+        {
+            int maxPosition = j;
+            for (int q = j + 1; q < matrixxx.GetLength(1); q++)
+            {
+                if (matrixxx[i, q] > matrixxx[i, maxPosition]) maxPosition = q;
+            }
+            int temp = matrixxx[i, j];
+            matrixxx[i, j] = matrixxx[i, maxPosition];
+            matrixxx[i, maxPosition] = temp;
+        }
+    }
+}
+
+Console.Write("Введите количество строк m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Sort(matrix);
+Console.WriteLine("Отсортированный двумерный массив: ");
+PrintMatrix(matrix);
---
+++ practicum8/homework/zadacha2/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void FillMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 21);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int minSum(int[,] matr)
+{
+    int[] summury = new int[matr.GetLength(0)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sum += matr[i, j];
+        }
+        summury[i] = sum;
+    }
+    Console.Write($"Суммы строк массива: ");
+    for (int i = 0; i < summury.Length; i++)
+    {
+        Console.Write($"{summury[i]}; ");
+    }
+    Console.WriteLine();
+    int minrow = 0;
+    for (int i = 1; i < summury.Length; i++)
+    {
+
+        if (summury[i] <= summury[minrow]) minrow = i; // если строк с наим суммой несколько сделать минроу массивом/строкой в который записываются строки
+    }
+    return minrow + 1;
+}
+
+Console.Write("Введите количество строк m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов m = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine($"Номер строки с наименьшей суммой: {minSum(matrix)}");
