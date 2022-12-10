0266addb571f6c2da89fac3caf0898ed6c852946
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2022-12-10 13:54:09 +0400

    homework 7 done

---
+++ practicum7/homework/zadacha3/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 21);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+Console.Write("Введите количество строк m = ");
+int m = Int32.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов n = ");
+int n = Int32.Parse(Console.ReadLine());
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+
+
+void Srednee(int[,] matr)
+{
+
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i, j];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {j + 1}: {Math.Round(sum / matr.GetLength(0)),2}; ");
+    }
+}
+Srednee(matrix);
